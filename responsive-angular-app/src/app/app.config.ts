d61cfef8409c2555dd16da84fd47ea0e64c56ca1
Matias Huartamendia <matias.huarta@gmail.com>
2026-04-17 02:15:27 -0300

    Second and Ready to Go Commit

--- responsive-angular-app/src/app/app.config.ts
+++ responsive-angular-app/src/app/app.config.ts
@@ -1,16 +1,8 @@
 import { ApplicationConfig } from '@angular/core';
-import { Routes, provideRouter } from '@angular/router';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
-
-const routes: Routes = [
-  // Example: { path: 'home', component: SomeHomeComponent },
-];
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
-    provideClientHydration(withEventReplay()),
     provideAnimations()
   ]
 };
